313ef92abf3a8a043207b7f5215a2c952c20c510
Leandro Martins <leandro.martins.85@gmail.com>
2021-12-19 19:10:39 -0300

    Refactoring Header + Banner

--- src/components/Banner/Index.tsx
+++ src/components/Banner/Index.tsx
@@ -1,33 +1,49 @@
-import { Flex, Heading, Text, VStack } from '@chakra-ui/react';
-import Image from 'next/image';
+import { Flex, Heading, Text, Box, Image } from '@chakra-ui/react';
 
 export function Banner() {
   return (
     <Flex
       w="100%"
-      h={335}
-      justifyContent="space-between"
+      h={['163px', '250px', '250px', '335px']}
       bgImage="url(/images/Background.png)"
       backgroundPosition="center"
       backgroundSize="cover"
+      bgSize="no-repeat"
     >
-      <Flex w="100%" justifyContent="space-evenly">
-        <VStack paddingTop="20" paddingLeft="36" align="flex-start" spacing="5">
-          <Heading color="#F5F8FA">5 Continentes,</Heading>
-          <Heading color="#F5F8FA">infinitas possibilidades.</Heading>
+      <Flex
+        w="100%"
+        justifyContent={['center', 'space-between']}
+        align="center"
+        mx="auto"
+        px={['4', '10', '15', '20', '36']}
+      >
+        <Box>
+          <Heading
+            color="gray.100"
+            fontWeight="500"
+            fontSize={['xl', '2xl', '2xl', '2xl', '4xl']}
+          >
+            5 Continentes,
+            <br /> infinitas possibilidades.
+          </Heading>
 
-          <Text color="#DADADA" maxWidth={524} w="100%" fontSize="xl">
+          <Text
+            color="gray.300"
+            mt="5"
+            fontSize={['0.8rem', 'xl']}
+            maxW={['100%', '100%', '100%', '550px']}
+          >
             Chegou a hora de tirar do papel a viagem que você sempre sonhou.
           </Text>
-        </VStack>
-        <Flex
-          paddingTop="20"
-          paddingRight="36"
-          align="flex-end"
-          marginBottom={-33}
-        >
-          <Image src="/images/Airplane.svg" height={270} width={417} />
-        </Flex>
+        </Box>
+
+        <Image
+          src="/images/Airplane.svg"
+          w={['300px', '300px', '300px', '430px']}
+          display={['none', 'none', 'block']}
+          transform="translateY(48px)"
+          ml="8"
+        />
       </Flex>
     </Flex>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,34 @@
-import { Flex, Image } from '@chakra-ui/react';
+import { Flex, Image, Grid } from '@chakra-ui/react';
 
 export function Header() {
   return (
-    <Flex as="header" w="100%" p="7" justifyContent="center" mx="auto">
-      <Image src="/images/Logo.png" />
+    <Flex
+      bg="white"
+      as="header"
+      w="100%"
+      mx="auto"
+      px="1rem"
+      justifyContent="center"
+      align="center"
+      h={['50px', '100px']}
+    >
+      <Grid
+        h="100%"
+        mx="auto"
+        w="100%"
+        maxW="1160px"
+        alignItems="center"
+        templateColumns="repeat(3, 1fr)"
+        justifyContent="center"
+        alignSelf="start"
+      >
+        <Image
+          src="/images/Logo.png"
+          w={['81px', '184px']}
+          justifySelf="center"
+          gridColumn="2"
+        />
+      </Grid>
     </Flex>
   );
 }
